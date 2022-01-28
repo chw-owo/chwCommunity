cfeeb736393ec730d4b39359903dc8ff7e8d6805
chw-owo <96677719+chw-owo@users.noreply.github.com>
2022-01-28 10:31:04 +0900

    Add files via upload

---
+++ src/main/java/com/example/chwblog/ChwblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.chwblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChwblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChwblogApplication.class, args);
+    }
+
+}
